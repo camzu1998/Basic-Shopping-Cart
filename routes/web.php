a89455b423319679c15f929b355296d82c1d70dd
camzu1998 <kamillanger4@gmail.com>
2022-03-05 14:34:09 +0100

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartProductController;
+use App\Http\Controllers\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Product URIs
+Route::get('/products', [ProductController::class, 'index']); //list all products in the catalog as a paginated list with at most 3 products per page
+
+Route::get('/product', [ProductController::class, 'create']); //form to add product to the catalog
+Route::post('/product', [ProductController::class, 'store']); //add product to the catalog
+
+Route::get('/product/{product_id}', [ProductController::class, 'edit']); //form to update product name / update product price
+Route::put('/product/{product_id}', [ProductController::class, 'update']); //update product name / update product price
+Route::delete('/product/{product_id}', [ProductController::class, 'destroy']); //remove product from the catalog
+//Cart Product URIs
+Route::get('/cart', [CartController::class, 'index']); //list all products in the cart
+Route::delete('/cart/{product_id}', [CartController::class, 'destroy']); //remove product from the cart
