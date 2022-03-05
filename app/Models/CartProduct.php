2eef0e6b45fabab1613b553afad1223cc605181e
camzu1998 <kamillanger4@gmail.com>
2022-03-05 14:48:42 +0100

    CartProduct model scopes

--- app/Models/CartProduct.php
+++ app/Models/CartProduct.php
@@ -25,4 +25,27 @@ class CartProduct extends Model
         'product_id' => false,
         'qty' => false,
     ];
+
+    /**
+     * Scope a query to only include defined cart.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  integer $cart_id
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeCart($query, $cart_id)
+    {
+        return $query->where('cart_id', '=', $cart_id);
+    }
+    /**
+     * Scope a query to only include defined product.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  integer $product_id
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeProduct($query, $product_id)
+    {
+        return $query->where('product_id', '=', $product_id);
+    }
 }
